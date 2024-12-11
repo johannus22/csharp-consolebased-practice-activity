bb6cb4bc3288d067747690a92145a427a9dcfe3e
johannus22 <jmasilungan1822@gmail.com>
2024-12-11 10:48:44 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+MultiDimensionalArray();
+Spacer();
+TraverseArrayForEachApproach();
+TraverseArrayForLoopApproach();
+
+
+static void MultiDimensionalArray(){
+    // This method traverses the numbers from a two domensional array
+        int?[,] numbers = { {1, 4, 2}, {3, 6, 8} };
+
+        for (int i = 0; i < numbers.GetLength(0); i++) 
+        { 
+            for (int j = 0; j < numbers.GetLength(1); j++) 
+            { 
+            Console.WriteLine(numbers[i, j]); 
+            } 
+        }
+}
+
+
+static void Spacer(){
+    Console.WriteLine("=============================");
+}
+
+
+static void TraverseArrayForEachApproach(){
+    int?[] numbers = {1,4,6,9,45,76,32,78,345,0,34,66,69};
+    foreach(int? i in numbers){
+        Console.WriteLine(i.Value);
+    }
+}
+
+
+static void TraverseArrayForLoopApproach(){
+    string?[] names = {
+        "Zhaira",
+        "Rafael",
+    };
+    for (int i = 0; i < names.Length; i++){
+        Console.WriteLine($"{names[i]}");
+    }
+}
+
+
